733b2684616e33be4fb9adb75efdbaa822f47a1c
Megha Bisht <bishtmegha98@gmail.com>
2018-12-31 18:40:22 +0530

    Set .symmetric_difference() Operation

---
+++ 30.py
@@ -0,0 +1,6 @@
+if __name__=='__main__':
+    n=int(input())
+    E=set(map(int,input().split()))
+    m=int(input())
+    F=set(map(int,input().split()))
+    print(len((E.symmetric_difference(F))))
