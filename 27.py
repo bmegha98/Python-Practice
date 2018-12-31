aa3fc8379f48c1bf7115121bb3e38b697c557c43
Megha Bisht <bishtmegha98@gmail.com>
2018-12-31 18:35:23 +0530

    Set .union() Operation

--- 27.py
+++ 27.py
@@ -3,8 +3,5 @@ if __name__=='__main__':
     E=set(map(int,input().split()))
     m=int(input())
     F=set(map(int,input().split()))
-    l=[]
-    l.extend(E.difference(F))
-    l.extend(F.difference(E))
-    l.extend(E.intersection(F))
-    print(len(l))
+    print(len((E.union(F))))
+    
