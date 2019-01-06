ae833ec28da81edcdc3562a3f700b417073db657
Megha Bisht <bishtmegha98@gmail.com>
2019-01-06 20:52:34 +0530

    Find the Torsional Angle

--- Classes/Class2.py
+++ Classes/Class2.py
@@ -16,7 +16,6 @@ class Points(object):
     def cross(self, no):
         return Points(self.y*no.z-self.z*no.y , self.z*no.x-self.x*no.z , self.x*no.y-self.y*no.x)
 
-        
     def absolute(self):
         return pow((self.x ** 2 + self.y ** 2 + self.z ** 2), 0.5)
  if __name__ == '__main__':
