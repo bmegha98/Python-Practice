7774369b78e486cfa8351d8ad73f598a2795c5da
Megha Bisht <bishtmegha98@gmail.com>
2019-01-29 20:12:08 +0530

    Update Number Of Open Doors.py

--- GeeksForGeeks/Number Of Open Doors.py
+++ GeeksForGeeks/Number Of Open Doors.py
@@ -31,6 +31,7 @@ if __name__=='__main__':
 '''
 '''
 Every door is visited by its factors and door is open if number of its factors are odd. Perfect square number has Odd number of factors.
+Number of perfect squares less than or equal to N is floor(sqrt(N)).
 '''
 import math
 if __name__=='__main__':
