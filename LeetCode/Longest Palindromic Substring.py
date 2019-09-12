cde8d6c0ce0201b2d6696621f877426da5b44d37
Megha Bisht <bishtmegha98@gmail.com>
2019-09-12 18:34:00 +0530

    Update Longest Palindromic Substring.py

--- LeetCode/Longest Palindromic Substring.py
+++ LeetCode/Longest Palindromic Substring.py
@@ -2,6 +2,7 @@
 Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
 '''
 
+'''
 class Solution(object):
     def longestPalindrome(self, s):
         """
@@ -17,4 +18,38 @@ class Solution(object):
                     return res
             size -= 1
         return ''
+'''
+def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        n = len(s)
+        maxlen = 1
+        low , high = 0 , 0
+        start = 0
+        for i in range(1,n):
+            #even length
+            low = i-1
+            high = i
+            while low >=0 and high <n and s[low] == s[high]:
+                if high-low+1 > maxlen:
+                    start = low
+                    maxlen = high-low+1
+                low -= 1
+                high += 1
+            #odd length
+            low = i-1
+            high = i+1
+            while low >=0 and high <n and s[low] == s[high]:
+                if high-low+1 > maxlen:
+                    start = low
+                    maxlen = high-low+1
+                low -= 1
+                high += 1
+        return s[start:start+maxlen]
+            
+
+
+
         
